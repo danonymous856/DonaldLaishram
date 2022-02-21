ae7ce88762a4288a4bed0eec526335bfc70f8dfd
danonymous856 <donaldlaishram861@gmail.com>
2022-02-21 20:39:16 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-componenets';
+
+const Index = props => {
+    return (
+        <div>
+            <Index>
+                <div>
+                    <p>WE will We will rock you
+                    Bloody you sre a hard guy fart guy driving sdince the Day all night yuo got Blood On yuor face a Big Disgrace drinking the Potion and Spitting All oveer the Face </p>
+                </div>
+            </Index>
+            <p>made in India !! </p>
+
+        </div>
+    );
+};
+
+Index.propTypes = {
+
+};
+
+export default Index;
